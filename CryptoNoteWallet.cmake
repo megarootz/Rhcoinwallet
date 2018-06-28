d111cdc9d84af5e7ac40f229aece9aa3e6a43eb1
megarootz <39173042+megarootz@users.noreply.github.com>
2018-06-29 03:29:54 +0800

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "trywallet")
+set(CN_CURRENCY_DISPLAY_NAME "trycoin")
+set(CN_CURRENCY_TICKER "try")
